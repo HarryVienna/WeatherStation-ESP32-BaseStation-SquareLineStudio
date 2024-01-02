7d2e7721229af1145fda1c18c7c78845e119728a
HarryVienna <mail@haraldkreuzer.net>
2024-01-02 11:35:02 +0100

    init

---
+++ SquareLine_Project_events.py
@@ -0,0 +1,48 @@
+
+def wifi_scan(event_struct):
+    return
+
+
+def store_settings(event_struct):
+    return
+
+
+def start_tasks(event_struct):
+    return
+
+
+def load_settings(event_struct):
+    return
+
+
+def event_wifi_scan(event_struct):
+    return
+
+
+def event_wifi_connect(event_struct):
+    return
+
+
+def event_load(event_struct):
+    return
+
+
+def event_start(event_struct):
+    return
+
+
+def event_screen_load_start(event_struct):
+    return
+
+
+def event_weatherstation_start(event_struct):
+    return
+
+
+def event_value_changed(event_struct):
+    return
+
+
+def event_screen_loaded(event_struct):
+    return
+
